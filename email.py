81821d1272658bd149273c4dfbad5e418a0f8a8e
HungTranVan01 <tranvanhung2622001@gmail.com>
2021-12-20 17:07:02 +0700

    test git

---
+++ email.py
@@ -0,0 +1,16 @@
+def emailProcess(email):
+    email_username = email[0:email.index("@")]
+    email_domain = email[email.index("@")+1:]
+    return [email_username, email_domain]
+
+def printMsg(email_username, email_domain):
+    print(f"Username is {email_username}; Email domain is {email_domain}")
+def main():
+    email = input("Nhap email cua ban :").strip()
+    email_username, email_domain = emailProcess(email)
+    printMsg(email_username,email_domain)
+
+if __name__ == "__main__":
+    main()
+
+        
